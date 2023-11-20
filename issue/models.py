852e33e160c5d34c1d5fa5672c22bce1942a6907
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-11-20 08:13:39 -0500

    Create the Developer model

--- issue/models.py
+++ issue/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from django.conf import settings
 
 
+class Developer(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    developer_statuses = [
+        ('junior', 'Junior'),
+        ('senior', 'Senior')
+    ]
+    status = models.CharField(max_length=6, choices=developer_statuses)
+    birth_date = models.DateField()
+    phone = models.CharField(max_length=20)
+
+    gender_choices = [
+        ('male', 'Male'),
+        ('female', 'Female')
+    ]
+    gender = models.CharField(max_length=6, choices=gender_choices)
+
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
 class Issue(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issues')
