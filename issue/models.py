98bb18e4b8075158eecf6aec31e7f2a892d1b4b1
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-11-20 08:00:17 -0500

    Add a one to many relationship between the user model and issue model

--- issue/models.py
+++ issue/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 
 class Issue(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issues')
     title = models.CharField(max_length=50)
     description = models.TextField()
 
