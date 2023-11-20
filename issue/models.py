3e147011244000179d7888a3111c1487e4072696
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-11-20 07:54:57 -0500

    Create the Issue model

--- issue/models.py
+++ issue/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    ISSUE_OPEN = 'OPEN'
+    ISSUE_IN_PROGRESS = 'IN_PROGRESS'
+    ISSUE_CLOSED = 'CLOSED'
+
+    issue_status_choices = [
+        (ISSUE_OPEN, 'Open'),
+        (ISSUE_IN_PROGRESS, 'In Progress'),
+        (ISSUE_CLOSED, 'Closed')
+    ]
+
+    status = models.CharField(
+        max_length=11, choices=issue_status_choices, default=ISSUE_OPEN)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
