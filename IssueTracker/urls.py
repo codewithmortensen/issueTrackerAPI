6d9bc7821983b31bc8f9c91f65ee281450744519
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-11-20 08:29:54 -0500

    Install fjango rest framework

--- IssueTracker/urls.py
+++ IssueTracker/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('api-auth/', include('rest_framework.urls')),
     path('issues/', include('issue.urls'))
 ]
--- issue/views.py
+++ issue/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view()
 def issue(request):
-    return render(request, 'issues/index.html')
+    return Response('ok')
